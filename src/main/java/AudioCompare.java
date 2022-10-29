5540d08002a80d6a54f23a43758383a006dc6019
Aadhitya A <aadhitya864@gmail.com>
2022-10-29 23:09:21 +0530

    Add test audio files

--- src/main/java/AudioCompare.java
+++ src/main/java/AudioCompare.java
@@ -2,13 +2,21 @@ import java.io.File;
 
 import org.tensorflow.Operation;
 import org.tensorflow.TensorFlow;
+import org.tensorflow.internal.c_api.TF_TString;
 import org.tensorflow.op.audio.DecodeWav;
 import org.tensorflow.op.audio.Mfcc;
+import org.tensorflow.op.io.ReadFile;
+import org.tensorflow.types.TString;
 
-public class AudioCompare implements Operation{
-    public void mfccFn(File f1, File f2) {
+/** 
+public class AudioCompare{
+    public void mfccFn(String f1, String f2) {
         System.out.println("Checking TensorFlow version...");
         System.out.println(TensorFlow.version());
-        DecodeWav dWav = DecodeWav.create(, f1, null);
+        TString wav1 = ReadFile.create(null, null);
+        DecodeWav dWav = DecodeWav.create(null, null, null);
+        DecodeWav dWav2 = DecodeWav.create(null, null, null);
+        dWav.audio();
     }
 }
+*/
